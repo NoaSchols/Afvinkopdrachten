9bf3a465df07ae5cdb22a781a7f2b4a9a743e93e
NoaSchols <55191728+NoaSchols@users.noreply.github.com>
2019-10-02 16:51:02 +0200

    Update chapter 4 opdracht 3.py

--- chapter 4 opdracht 3.py
+++ chapter 4 opdracht 3.py
@@ -1,11 +1,13 @@
+#De waarden hieronder wordt overschreven.
 rondje = 0
 
 print (" We gaan rondjes rennen! ")
-
+#De range gaat van 1 tot en met 5, je kan 5 rondjes rennen.
 for rondje in range(1,6):
     rondje += rondje
     int(input("Hoeveel seconde duurde je rondje" ))
 
+#Hij print de totaalaantal rondjes, de kortste,langste en de gemiddelde tijd.
 print (rondje)
 kortste = int(input(" Hoeveel seconde duurde je kortste ronde? "))
 langste = int(input(" Hoeveel seconde duurde je langste ronde? "))
@@ -14,7 +16,7 @@ gemiddelde = rondje/6
 print (kortste)
 print (langste)
 print (gemiddelde)
-
+#Eind van het script.
 
 
 
