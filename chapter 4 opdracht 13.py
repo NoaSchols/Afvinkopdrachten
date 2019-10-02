6b80dc9e31f05596b78f92da1c77770dba0b5978
NoaSchols <55191728+NoaSchols@users.noreply.github.com>
2019-10-02 16:49:06 +0200

    Update chapter 4 opdracht 13.py

--- chapter 4 opdracht 13.py
+++ chapter 4 opdracht 13.py
@@ -1,16 +1,20 @@
+#De waarden hieronder worden overschreven
 panda = 0
 panda = int(input( " Hoeveel pandas zijn er op dag 1? "))
 dagen = "ja"
 dag = 0
 dag2 = 0
-
+#Zolang while is gelijk ja blijft hij vragen welke dag is het vandaag.
 while dagen == "ja" :
+#Bij dagen alleen 1,2,3 enz invullen.
     input (" Het is vandaag dag ")
     dag = ((panda * 100) / 30 )
     dag2 += dag  
     print ( dag2)
 
+#Alle dagen worden opgeteld en dag2 genoemd.
+
 
    dagen = input ( " Wil de doorgaan (ja/nee)? " )
-    
+#Zolang je ja typt ga je door anders stop je.
     
